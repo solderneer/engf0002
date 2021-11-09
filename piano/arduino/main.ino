bd2f0d69df67a156d9138e036eccdd9acfeb4041
Sudharshan <sudhar393@gmail.com>
2021-11-09 20:24:30 +0000

    feat: streaming logic and basic functionality

--- piano/arduino/main.ino
+++ piano/arduino/main.ino
@@ -4,15 +4,54 @@
   This example code is in the public domain.
  */
 
+#define YELLOW_BUTTON (11)
+#define BLUE_BUTTON (10)
+#define GREEN_BUTTON (9)
+#define RED_BUTTON (8)
+#define BUZZER (6) // Needs PWM
+
+uint8_t a_buffer[4] = {0, 0, 0 ,0};
+uint8_t buttons[] = {YELLOW_BUTTON, BLUE_BUTTON, GREEN_BUTTON, RED_BUTTON};
+uint32_t button_tones[] = {262, 294, 330, 350};
+
+/****** UTILITIES *********/
+
+// Takes a 4 byte uint8_t array and converts it to a 32 bit unsigned integer
+uint32_t bufferToUInt32(uint8_t *b) {
+  return ((uint32_t)b[0] << 24) | ((uint32_t)b[1] << 16) | ((uint32_t)b[2] << 8) | (uint32_t)b[3];
+}
+
+
+/********* MAIN CODE **************/
 void setup() {                
-	// initialize the digital pin as an output.
-	// Pin 13 has an LED connected on most Arduino boards:
-	pinMode(13, OUTPUT);     
+  // Initialise the pushbuttons
+	pinMode(buttons[0], INPUT_PULLUP);
+  pinMode(buttons[1], INPUT_PULLUP);
+  pinMode(buttons[2], INPUT_PULLUP);
+  pinMode(buttons[3], INPUT_PULLUP);
+  pinMode(BUZZER, OUTPUT);
+
+  // Initialise the serial communication
+  Serial.begin(9600);
 }
 
 void loop() {
-	digitalWrite(13, HIGH);   // set the LED on
-	delay(500);              // wait for a second
-	digitalWrite(13, LOW);    // set the LED off
-	delay(500);              // wait for a second
+  // First check and play any input data
+  if(Serial.available() > 0) {
+    Serial.readBytesUntil('\n', a_buffer, 4);
+    // uint32_t freq = ((uint32_t)a_buffer[0] << 24) | ((uint32_t)a_buffer[1] << 16) | ((uint32_t)a_buffer[2] << 8) | (uint32_t)a_buffer[3];
+    uint32_t freq = bufferToUInt32(a_buffer);
+    Serial.print(freq);
+    tone(BUZZER, freq, 1000); 
+  }
+
+  // Sample the buttons for tones
+  int i;
+  for(i = 0; i < 4; i++) {
+    if(digitalRead(buttons[i]) == 0){
+      tone(BUZZER, button_tones[i], 200);
+      delay(100);
+      noTone(BUZZER);
+    }
+  }
 }
